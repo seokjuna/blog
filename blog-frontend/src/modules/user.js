cb6ac394ef64ccbf53d717ff57b7c7798c4659e6
naseokju <lukas95@naver.com>
2022-12-26 18:07:10 +0900

    login 검증 실패 시 정보  초기화

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -13,8 +13,18 @@ export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+// CHECK_FAILURE 액션이 발생할 때
+function checkFailureSaga() {
+    try {
+        localStorage.removeItem('user'); // localSotrage에서 user 제거
+    } catch (e) {
+        console.log('localStorage is not working');
+    }
+}
 export function* userSaga() {
     yield takeLatest(CHECK, checkSaga);
+    yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
 
 const initialState = {
