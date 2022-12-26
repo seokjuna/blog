7473083794a92800c2aa9bcddcff723569e2d3c6
naseokju <lukas95@naver.com>
2022-12-26 14:11:25 +0900

    회원가입, 로그인 컨테이너 및 axios, proxy

--- blog-frontend/src/pages/LoginPage.js
+++ blog-frontend/src/pages/LoginPage.js
@@ -1,10 +1,10 @@
-import AuthForm from "../components/auth/AuthForm";
 import AuthTemplate from "../components/auth/AuthTemplate";
+import LoginForm from "../containers/auth/LoginForm";
 
 const LoginPage = () => {
     return (
         <AuthTemplate>
-            <AuthForm type="login" />
+            <LoginForm />
         </AuthTemplate>
     );
 };
