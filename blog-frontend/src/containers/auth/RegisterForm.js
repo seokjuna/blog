41fde5b6574c5ad051151bb71d82d2d0a3405809
naseokju <lukas95@naver.com>
2022-12-26 16:00:54 +0900

    register 구현

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AuthForm from "../../components/auth/AuthForm";
-import { changeField, initializeForm } from "../../modules/auth";
+import { changeField, initializeForm, register } from "../../modules/auth";
+import { check } from "../../modules/user";
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.register
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user
     }));
+    // 회원가입 성공 후 홈화면으로 라우트 이동
+    const navigate = useNavigate();
     // 인풋 변경 이벤트 핸들러
     const onChange = e => {
         const { value, name } = e.target;
@@ -23,13 +30,40 @@ const RegisterForm = () => {
     // 폼 등록 이벤트 핸들러
     const onSubmit = e => {
         e.preventDefault();
-        // 구현 예정
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: 오류 처리
+            return;
+        }
+        dispatch(register({ username, password }));
     };
 
     // 컴포넌트가 처음 렌더링될 때 form을 초기화함
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    // 회원가입 성공/실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+            // check를 호출하여 현재 사용자가 로그인 상태인지 확인
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    // user 값이 잘 설정되었는지 확인
+    useEffect(() => {
+        if (user) {
+            navigate('/'); // 홈 화면으로 이동
+        }
+    }, [navigate, user]);    
 
     return (
         <AuthForm
