b0b508dae88137e8f5040fa259136d86e06f7f91
naseokju <lukas95@naver.com>
2022-12-27 15:24:35 +0900

    write redux, EditorContainer

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,12 +1,12 @@
 import Responsive from "../components/common/Responsive";
-import Editor from "../components/write/Editor";
 import TagBox from "../components/write/TagBox";
 import WriteActionButtons from "../components/write/WriteActionButtons";
+import EditorContainer from "../containers/write/EditorContainer";
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
+            <EditorContainer />
             <TagBox />
             <WriteActionButtons />
         </Responsive>
