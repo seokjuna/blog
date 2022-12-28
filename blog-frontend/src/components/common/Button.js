bfdbc92b37fa0b50178e147b88ed72c0df9c51b5
naseokju <lukas95@naver.com>
2022-12-28 16:56:05 +0900

    pagination

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -33,7 +33,13 @@ const buttonStyle = css`
             &:hover {
                 background: ${palette.cyan[4]};
             }
-        `}    
+        `}  
+    
+    &:disabled {
+        background: ${palette.gray[3]};
+        color: ${palette.gray[5]};
+        cursor: not-allowed;
+    }
 `;
 
 const StyledButton = styled.button`
